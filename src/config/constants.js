408e0f677e290dc46914b2725ce89a3648b6664a
JefferDevop <somadesarrollo@gmail.com>
2025-08-28 19:06:52 -0500

    Change base_api

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 
- export const BASE_API = "https://glowing.catalogointeractivo.com.co"
+ export const BASE_API = "https://glowing.conexion.bar"
 
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart";
